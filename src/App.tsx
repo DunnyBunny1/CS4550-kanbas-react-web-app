75d87b1e211710328fa14164087597f29c3b33bc
DunnyBunny1 <murray.don@northeastern.edu>
2024-09-10 20:35:47 -0400

    Added labs 1-6 to demo branch

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Web Dev</h1>
-      <h2>Lab 5</h2>
+      <h2>Lab 1</h2>
       <h2>Lab 2</h2>
       <h2>Lab 3</h2>
       <h2>Lab 4</h2>
+      <h2>Lab 5</h2>
+      <h2>Lab 6</h2>
     </div>
   );
 }
